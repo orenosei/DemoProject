8962de76f13a928544b7997ec98861f2019df771
orenosei <thanhngoac2005@gmail.com>
2025-02-28 23:50:06 +0700

    update

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module sample.demoproject {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens sample.demoproject to javafx.fxml;
+    exports sample.demoproject;
+}
